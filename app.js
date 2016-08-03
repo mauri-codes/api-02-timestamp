391354663648df839bcd45a023c86054d3c30e57
mauri1789 <mauricio.alarcon1789@gmail.com>
2016-08-03 18:22:08 -0400

    os variable modified

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ var express = require("express");
 
 var app = express();
 var os = require('os');
+var parser = require('ua-parser-js');
 var requestIp = require("request-ip");
 
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", function (req, res) {
-	//var ip = req.ip;
 	var ip = requestIp.getClientIp(req);
-	console.log(ip);
+	var ua = parser(req.headers['user-agent']).ua;
+	var ini = ua.indexOf("(");
+	var fin = ua.indexOf(")")-ini;
+	var subs = ua.substr(ini+1, fin);
 	var lan = req.headers["accept-language"].split(";");
 	var rel = os.release().split(".");
-	var ops = os.type() + " " + rel[0] + "." + rel[1] + "; " + os.arch();
+	var ops = subs;
 	res.json({"ipaddress": ip, "language": lan[0], "software": ops});
 });
 
