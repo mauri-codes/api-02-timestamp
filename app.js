57f94701ac0a79dfc1de2f3bc147151e0f1c52eb
mauri1789 <mauricio.alarcon1789@gmail.com>
2016-08-03 17:21:37 -0400

    using request-ip

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require("express");
 
 var app = express();
 var os = require('os');
+var requestIp = require("request-ip");
+
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", function (req, res) {
-	var ip = req.ip;
+	//var ip = req.ip;
+	var ip = requestIp.getClientIp(req);
+	console.log(ip);
 	var lan = req.headers["accept-language"].split(";");
 	var rel = os.release().split(".");
 	var ops = os.type() + " " + rel[0] + "." + rel[1] + "; " + os.arch();
